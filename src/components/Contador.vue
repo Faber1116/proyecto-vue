5cbcf5d4766da4e30ec30eaceeaaf1757f58d9e6
Faber De arco <faberthebiznation@gmail.com>
2025-10-06 10:13:27 -0500

    componente creador

---
+++ src/components/Contador.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Hola mundo</h1>
+  <p>Contador: {{ contador }}</p>
+  <button @click="incrementar">Incrementar</button>
+  <button @click="disminuir">Disminuir</button>
+
+</template>
+
+<script setup>
+import { ref } from "vue";
+const contador = ref(0);
+const incrementar = () => {
+  contador.value++;
+}
+const disminuir = () => {
+  contador.value--;
+}
+</script>
+
+<style scoped>
+p {
+  color: red;
+}
+</style>
