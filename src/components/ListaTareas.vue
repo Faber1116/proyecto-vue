8423d250b631dd39f909d01cfac9cf2aba303d4e
Faber De arco <faberthebiznation@gmail.com>
2025-10-06 15:22:49 -0500

    sidebar

---
+++ src/components/ListaTareas.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h2>Lista de tareas</h2>
+        <input type="text" v-model="nuevaTarea" @keyup.enter="agregarTarea"><button
+            @click="agregarTarea">Agregar</button>
+        <ul>
+            <li v-for="tarea in tareas">{{ tarea }} <button
+                    @click="eliminarTarea(tareas.indexOf(tarea))">Eliminar</button></li>
+        </ul>
+    </div>
+</template>
+<script setup>
+import { ref } from "vue";
+
+const tareas = ref([]);
+const nuevaTarea = ref("");
+
+const agregarTarea = () => {
+    if (nuevaTarea.value) {
+        tareas.value.push(nuevaTarea.value);
+        nuevaTarea.value = "";
+    } else {
+        alert("Ingrese una tarea");
+    }
+};
+
+const eliminarTarea = (index) => {
+    tareas.value.splice(index, 1);
+};
+
+</script>
+<style scoped></style>
